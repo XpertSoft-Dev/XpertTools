d74b97f1a386cd5ca58abd9c3629b2e71f0a20ba
hamza khflaoui <h.khalfaoui@xpertsoft-dz.com>
2022-11-07 14:11:08 +0100

    Update BlazorApplication.cs

--- XpertTools.Blazor.Server/BlazorApplication.cs
+++ XpertTools.Blazor.Server/BlazorApplication.cs
@@ -9,42 +9,22 @@ using XpertTools.Blazor.Server.Services;
 
 namespace XpertTools.Blazor.Server;
 
-public class XpertToolsBlazorApplication : BlazorApplication {
-    public XpertToolsBlazorApplication() {
+public class XpertToolsBlazorApplication : BlazorApplication
+{
+    public XpertToolsBlazorApplication()
+    {
         ApplicationName = "XpertTools";
         CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
         DatabaseVersionMismatch += XpertToolsBlazorApplication_DatabaseVersionMismatch;
     }
-    protected override void OnSetupStarted() {
+    protected override void OnSetupStarted()
+    {
         base.OnSetupStarted();
-#if DEBUG
-        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
-            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
-        }
-#endif
+        DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
     }
-    private void XpertToolsBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
-#if EASYTEST
+    private void XpertToolsBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e)
+    {
         e.Updater.Update();
         e.Handled = true;
-#else
-       // if(System.Diagnostics.Debugger.IsAttached) 
-        {
-            e.Updater.Update();
-            e.Handled = true;
-        }
-        //else {
-        //    string message = "The application cannot connect to the specified database, " +
-        //        "because the database doesn't exist, its version is older " +
-        //        "than that of the application or its schema does not match " +
-        //        "the ORM data model structure. To avoid this error, use one " +
-        //        "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
-
-        //    if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
-        //        message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
-        //    }
-        //    throw new InvalidOperationException(message);
-        //}
-#endif
     }
 }
